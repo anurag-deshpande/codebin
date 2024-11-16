84ced2660b9ae1f6cb961262a60c9708e86afd99
Anurag Deshpande <55806832+anurag-deshpande@users.noreply.github.com>
2024-11-16 11:31:33 +0530

    Update firebase-config.ts
    updated firebas.config.ts

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyCVAkotdo6Xzovn4eM-i7OlzTalL7-jxXM",
-  authDomain: "codebin-ng-dac86.firebaseapp.com",
-  projectId: "codebin-ng-dac86",
-  storageBucket: "codebin-ng-dac86.firebasestorage.app",
-  messagingSenderId: "283680498848",
-  appId: "1:283680498848:web:f34270c24152e3c203d5c5"
+  apiKey: "your API key here...",
+  authDomain: "auth domain here...",
+  projectId: "project id here...",
+  storageBucket: "storage bucket here....",
+  messagingSenderId: "messagingSenderId here....",
+  appId: "app id here..."
 };
 
 // Initialize Firebase
